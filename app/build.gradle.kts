34c8f67949fed42de3415bf730ab42f913b8b190
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2026-01-08 00:16:48 +0900

    Meta: Version 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sasakulab.yure_android_client"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.4.0"
+        versionCode = 2
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
