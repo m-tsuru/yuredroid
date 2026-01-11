1eb2e93d6b06f0a85c5685d7cd3687262295b0b5
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2026-01-12 02:56:33 +0900

    Meta: Version 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sasakulab.yure_android_client"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.4.2"
+        versionCode = 3
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
